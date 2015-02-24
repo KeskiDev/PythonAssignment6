e8cd90989614ae42d1221fe126f8c1fddb7812c0
PikkuLy <komeaolsen@gmail.com>
2015-02-24 09:48:37 -0700

    added python code

---
+++ HW6.py
@@ -0,0 +1,9 @@
+import pygame,sys
+pygame.init()
+
+def run_game():
+    screen = pygame.display.set_mode(640,480)
+    pygame.display.set_caption("Colliding!")
+
+    ##blue or black background image
+    ##tardis is the one user controls
